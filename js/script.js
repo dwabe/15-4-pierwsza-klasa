6f6cb938ee8e43a6bd55cc922a0d6ee6b0c4e910
Bartek Błęcki <bartek@blecki.pl>
2019-05-31 23:57:40 +0200

    Update script.js - add function phone

---
+++ js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function phone(brand, price, color, memory, premiere) {
+    this.brand = brand;
+    this.price = price;
+    this.color = color;
+    this.memory = memory;
+    this.premiere = premiere;
+}
