9c44f840e4f16f588e69ff30e599e5ea0553fe08
Bartek Błęcki <bartek@blecki.pl>
2019-06-01 00:15:02 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 'use strict';
 
-function phone(brand, price, color, memory, premiere) {
+function Phone(brand, price, color, memory, premiere) {
     this.brand = brand;
     this.price = price;
     this.color = color;
     this.memory = memory;
     this.premiere = premiere;
 }
+
+Phone.prototype.printInfo = function () {
+    console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ". This phone has " + this.memory + " of memory. Its premiere took place in a year " + this.premiere);
+}
+
+var iPhone6S = new Phone("Apple", 1350, "silver", "16GB", 2015);
+var GalaxyS6 = new Phone("Samsung", 1100, "black", "32GB", 2015);
+var OnePlusOne = new Phone("OnePlus", 900, "white", "16GB", 2015);
+
+iPhone6S.printInfo();
+GalaxyS6.printInfo();
+OnePlusOne.printInfo();
